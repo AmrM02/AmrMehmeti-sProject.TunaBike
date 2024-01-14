e7732f40628fa865e53c1dea211b11685af93f03
Amer Mehmeti <156483786+AmrM02@users.noreply.github.com>
2024-01-14 22:39:01 +0100

    Add files via upload

---
+++ AmrContacts.js
@@ -0,0 +1,8 @@
+AmrContacts.js
+
+function submitForm() {
+      
+      alert("Signup successful! Redirecting to the homepage...");
+     
+      window.location.href = "file:///Users/administrator/Documents/U1/Amr.html?#";
+    }
