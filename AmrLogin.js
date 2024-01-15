1d8d2618b910b4022290bd747e61a93739995f36
Amer Mehmeti <156483786+AmrM02@users.noreply.github.com>
2024-01-15 12:45:18 +0100

    Add files via upload

---
+++ AmrLogin.js
@@ -0,0 +1,10 @@
+AmrLogin.js
+
+AmrSignUp.js
+
+function submitForm() {
+      
+      alert("Login successful! Redirecting to the homepage...");
+     
+      window.location.href = "file:///Users/administrator/Documents/U1/Index.html";
+    }
---
+++ AmrSignUp.js
@@ -0,0 +1,8 @@
+AmrSignUp.js
+
+function submitForm() {
+      
+      alert("Signup successful! Redirecting to the homepage...");
+     
+      window.location.href = "file:///Users/administrator/Documents/U1/Index.html";
+    }
