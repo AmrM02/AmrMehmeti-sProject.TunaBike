edabe684e07dafdb127efbbf214138a768eb383d
Amer Mehmeti <156483786+AmrM02@users.noreply.github.com>
2024-01-15 12:25:01 +0100

    Delete AmrContacts.js

--- AmrContacts.js
+++
@@ -1,8 +0,0 @@
-AmrContacts.js
-
-function submitForm() {
-      
-      alert("Signup successful! Redirecting to the homepage...");
-     
-      window.location.href = "file:///Users/administrator/Documents/U1/Index";
-    }
